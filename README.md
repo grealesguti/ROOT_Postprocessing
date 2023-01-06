f349527d8f26b56cc3d23fc200502f39544b4a89
greales <g.realesguti@gmail.com>
2023-01-06 10:59:47 +0100

    add: plot Z vs LO

---
+++ README.md
@@ -0,0 +1,62 @@
+# README
+
+## Usage
+
+root 'plotHistxxx("folderpathinTierIIData",HistXmin,HistXmax,Nbins,"savefolder")'
+root 'plotGraphxxx("folderpathinTierIIData",HistXmin,HistXmax,Nbins,"savefolder")'
+
+
+## Trees and Branches in the root files
+
+/* Tree and Tuple values from G4
+ * 	* Arrivals
+ * 		- fEvent
+ * 		- fX
+ * 		- fY
+ * 		- fZ
+ * 		- fWlen
+ * 		- fMeanPath
+ * 	* Detected
+ * 		- fEvent
+ * 		- ftrackID
+ * 		- fX
+ * 		- fY
+ * 		- fZ
+ * 		- ftimeG
+ * 		- ftimeL
+ * 		- fPDE
+ * 		- fwlen
+ * 		- fMeanPath
+ * 	* Stepping
+ * 		- fKilledLength
+ * 		- ftime
+ * 		- fX
+ * 		- fY
+ * 		- fZ
+ * 	* EndOfEvent
+ * 		- fEdep
+ * 		- fPhot
+ * 		- fHits
+ * 		- fPDE
+ * 		- fLOapprox
+ * 		- fPrimaryX
+ * 		- fPrimaryZ
+ * 		- fGlueL
+ * 		- fResinL
+ * 		- fDetXpos
+ * 		- fDetXpos2
+ * 		- fCrossTalkCount
+ * 		- fCrossTalkMev
+ * 		- fTiming
+ * 		- fphL
+ * 		- fphR
+ * 		- fevt
+ * 	* EndofRun
+ * 		- fLOAvg
+ * 		- fLOStd
+ * 		- fTimAvg
+ * 		- fTimStd
+ * 		- frun
+ * 		- fincr
+*/
+
---
+++ plotGraphEndOfEventPrimaryZvsLO.cc
@@ -0,0 +1,23 @@
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <string>
+//#include "src/plotRootScatterGraph.cc"
+#include "src/getCurrentDateTimeString.cc"
+#include "src/plotRootScatterGraph_ZvsLO.cc"
+
+void plotGraphEndOfEventPrimaryZvsLO(const std::string& folder,double yMax = 6500,
+									 const std::string& saveFolder = "Figures/ZvsLO/")
+{
+	// Get current date and time
+	std::string currentDateTime = getCurrentDateTimeString();
+	std::cout << currentDateTime << std::endl;
+
+    // Plot data from EndOfEvent tree
+    std::cout << "Data folder: "<< folder << std::endl;
+    //plotRootScatterGraph("../TierIIData/"+folder, "EndOfEvent", "fPrimaryZ", "EndOfEvent", "fphL", "EndOfEvent_fPrimaryZ_vs_fLO_" + folder + currentDateTime,-29,29,0,yMax,saveFolder);
+	plotRootScatterGraph_ZvsLO("../TierIIData/"+folder, "EndOfEvent", "fPrimaryZ", "EndOfEvent", "fphL","EndOfEvent", "fphR",
+								"EndOfEvent_fPrimaryZ_vs_fLO_" + folder + currentDateTime,-29,29,0,yMax,saveFolder);
+}
+//fPrimaryZ
+//fLOapprox
